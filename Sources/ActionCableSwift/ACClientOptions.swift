2cedd7a0c703c4bf597ba40ab022f923c0d2536c
High5Apps <high5apps@gmail.com>
2020-09-11 11:49:27 -0700

    Remove unused debug option from ACClientOptions

--- Sources/ActionCableSwift/ACClientOptions.swift
+++ Sources/ActionCableSwift/ACClientOptions.swift
@@ -8,18 +8,9 @@
 import Foundation
 
 public struct ACClientOptions {
-    #if DEBUG
-    public var debug = true
-    #else
-    public var debug = false
-    #endif
+    let reconnect: Bool
 
-    public var reconnect: Bool = false
-
-    public init() {}
-
-    public init(debug: Bool, reconnect: Bool) {
-        self.debug = debug
+    public init(reconnect: Bool = false) {
         self.reconnect = reconnect
     }
 }
