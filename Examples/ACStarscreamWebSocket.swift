6d3643ff8de4254e5978fd4afc36e5c103be3dd6
High5Apps <high5apps@gmail.com>
2020-09-21 10:47:21 -0700

    Extract WebSocketDelegate protocol into an extension

--- Examples/ACStarscreamWebSocket.swift
+++ Examples/ACStarscreamWebSocket.swift
@@ -9,19 +9,18 @@ import Foundation
 import ACActionCable
 import Starscream
 
-class ACStarscreamWebSocket: ACWebSocketProtocol, WebSocketDelegate {
+class ACStarscreamWebSocket: ACWebSocketProtocol {
     
     var url: URL
+    var onConnected: ACConnectionHandler?
+    var onDisconnected: ACDisconnectionHandler?
+    var onText: ACTextHandler?
     
     private var socket: WebSocket?
 
     init(stringURL: String) {
         url = URL(string: stringURL)!
     }
-
-    var onConnected: ACConnectionHandler?
-    var onDisconnected: ACDisconnectionHandler?
-    var onText: ACTextHandler?
 
     func connect(headers: ACRequestHeaders?) {
         guard socket == nil else { return }
@@ -38,7 +37,10 @@ class ACStarscreamWebSocket: ACWebSocketProtocol, WebSocketDelegate {
     func send(text: String, completion: ACEventHandler?) {
         socket?.write(string: text, completion: completion)
     }
+}
 
+extension ACStarscreamWebSocket: WebSocketDelegate {
+    
     func didReceive(event: WebSocketEvent, client: WebSocket) {
         switch event {
         case .connected(let headers):
